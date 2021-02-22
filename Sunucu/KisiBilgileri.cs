c424f71867a65802d51852d1659e0bc5549325ae
Kerem Ayseli <46223333+KeremAyseli@users.noreply.github.com>
2021-02-22 06:46:32 +0300

    sunucu eklendi

---
+++ Sunucu/KisiBilgileri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sunucu
+{
+    class KisiBilgileri
+    {
+        public int id { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+
+    }
+}
